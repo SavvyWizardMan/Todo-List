649b249cf87a16eda0fbdf99eda77044643a52a8
SavvyWizard <174541572+SavvyWizardMan@users.noreply.github.com>
2024-08-30 23:23:49 -0700

    test run

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import dom from "./domscript";
+import "./domscript";
 
 (function() {
     const container = document.querySelector('.container');
@@ -7,7 +7,40 @@ import dom from "./domscript";
     const addTaskBtn = document.querySelector('.add');
     const dialog = document.querySelector('dialog');
     const closeBtn = document.querySelector('.close');
+    const addTask = document.querySelector('.addTask');
+    const title = document.querySelector('input[id="title"]')
+    const desc = document.querySelector('input[id="desc"]')
+    const date = document.querySelector('input[type="date"]')
+    const radio1 = document.querySelector('input[id="high"]');
+    const radio2 = document.querySelector('input[id="med"]');
+    const radio3 = document.querySelector('input[id="low"]');
+    const inputs = [title, desc];
+    const radios = [radio1, radio2, radio3];
 
+    addTask.addEventListener('click', () => {
+        for (let i of inputs) {
+            if (i.value === "") return;
+        }
+        let isChecked = false;
+        for (let j of radios) {
+            if (j.checked) {
+                isChecked = true;
+                break;
+            }      
+        }
+
+        if (!isChecked) {
+            return;
+        }
+
+        title.value = "";
+        desc.value = "";
+        radio1.checked = false;
+        radio2.checked = false;
+        radio3.checked = false;
+        dialog.close();
+    });
+    
     section.addEventListener('mousemove', (e) => {
         const x = e.clientX;
         const y = e.clientY;
